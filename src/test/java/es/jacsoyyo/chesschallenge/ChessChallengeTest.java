f7c97072ce017c0674e72c58b7b55955ae457186
JacSoyYo <chamorro.j@gmail.com>
2015-05-15 21:41:04 +0200

    Test proposed examples and 7-Queens (only number of results)

---
+++ src/test/java/es/jacsoyyo/chesschallenge/ChessChallengeTest.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package es.jacsoyyo.chesschallenge;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author jacobo
+ */
+public class ChessChallengeTest {
+    
+    public ChessChallengeTest() {
+    }
+    
+    @Test
+    public void test7Queens7x7() {
+        int rows = 7;
+        int columns = 7;
+        List<String> pieces = new ArrayList<>(Arrays.asList("Q", "Q", "Q", "Q", "Q", "Q", "Q"));
+        ChessChallenge chessChallenge = new ChessChallenge(rows, columns, pieces);
+        assertEquals(chessChallenge.doChallenge(), 40);
+    }
+    
+    @Test
+    public void test2Kings1Rook3x3() {
+        int rows = 3;
+        int columns = 3;
+        List<String> pieces = new ArrayList<>(Arrays.asList("K", "K", "R"));
+        ChessChallenge chessChallenge = new ChessChallenge(rows, columns, pieces);
+        assertEquals(chessChallenge.doChallenge(), 4);
+    }
+
+    @Test
+    public void test2Rooks4Knights4x4() {
+        int rows = 4;
+        int columns = 4;
+        List<String> pieces = new ArrayList<>(Arrays.asList("R", "R", "N", "N", "N", "N"));
+        ChessChallenge chessChallenge = new ChessChallenge(rows, columns, pieces);
+        assertEquals(chessChallenge.doChallenge(), 8);
+    }
+}
